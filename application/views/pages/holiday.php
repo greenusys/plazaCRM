b30e3a289addf492ea2a3c68b28ac18b46d9d5ee
root <root@localhost.localdomain>
2020-02-19 12:45:53 +0530

    rav

--- application/views/pages/holiday.php
+++ application/views/pages/holiday.php
@@ -3,11 +3,11 @@
 {
   background-color:white;
 }
-.active, .back:hover
+/*.active, .back:hover
 {
   background-color: #23b7e5 !important;
   color:white;
-}
+}*/
 </style>
          <style>
           .input_year {
@@ -57,6 +57,23 @@
     background-color: #7266ba;
     border-color: transparent;
 }
+.color-tag {
+    display: inline-block;
+    width: 13px;
+    height: 13px;
+    margin: 2px 10px 0 0;
+    transition: all 300ms ease;
+}
+.clickable {
+    cursor: pointer;
+}
+.color-tag.active {
+    border-radius: 50%;
+}
+.month_tbs .active{
+  background: #6777ef;
+    color: white;
+}
 </style>
           <div class="row mt-4">
             <div class="col-lg-12">
@@ -65,18 +82,24 @@
         <div class="row mt-5">
           <div class="col-md-3">
                  <div class="yetr__">
+                  <form method="POST" action="<?=base_url()?>Utilities/holiday">
                     <ul class="list-unstyled d-flex">
                       <li ><strong>Year :</strong></li>
                       <li>
-                          <div class='input-group date datetimepicker10 w-75' id='datetimepicker10'>
-                              <input type='text' class="input_year form-control" />
+                          <div class='input-group date datetimepicker11 w-75' id='datetimepicker11'>
+                              <input type='text' class="input_year form-control" data-format="yyyy" name="year" value="<?php
+                if (!empty($year)) {
+                    echo $year;
+                }
+                ?>" />
                                 <span class="input-group-addon">
                                   <span ><i class="fa fa-calendar"></i></span>
                               </span>
                           </div>
                       </li>
-                      <li><button class="btn btn-info"><i class="fa fa-search" aria-hidden="true"></i></button></li>
+                      <li><button class="btn btn-info" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button></li>
                     </ul>
+                  </form>
                 </div>
               </div>
               <div class="col-md-7">
@@ -92,90 +115,112 @@
               </div>
             </div>
           <div class="row " >
-         
-              <!-- <div class="card"></div> -->
-              <div class="col-md-3">
-       
 
-              <div class="card">
-                <ul class="list-unstyled">
-                  <li>
-                    <a href="" class="mnth_c border-bottom"><i class="fas fa-calendar-alt"></i> <strong> January</strong> </a>
-                  </li>
-                   <li>
-                    <a href="" class="mnth_c border-bottom"><i class="fas fa-calendar-alt"></i> <strong> February</strong> </a>
-                  </li>
-                  <li>
-                    <a href="" class="mnth_c border-bottom"><i class="fas fa-calendar-alt"></i> <strong> March </strong> </a>
-                  </li>
-                  <li>
-                    <a href="" class="mnth_c border-bottom"><i class="fas fa-calendar-alt"></i> <strong> April </strong> </a>
-                  </li>
-                  <li>
-                    <a href="" class="mnth_c border-bottom"><i class="fas fa-calendar-alt"></i> <strong> May</strong> </a>
-                  </li>
-                  <li>
-                    <a href="" class="mnth_c border-bottom"><i class="fas fa-calendar-alt"></i> <strong> June</strong> </a>
-                  </li>
-                  <li>
-                    <a href="" class="mnth_c border-bottom"><i class="fas fa-calendar-alt"></i> <strong> July</strong> </a>
-                  </li>
-                  <li>
-                    <a href="" class="mnth_c border-bottom"><i class="fas fa-calendar-alt"></i> <strong> August</strong> </a>
-                  </li>
-                  <li>
-                    <a href="" class="mnth_c border-bottom"><i class="fas fa-calendar-alt"></i> <strong> September</strong> </a>
-                  </li>
-                  <li>
-                    <a href="" class="mnth_c border-bottom"><i class="fas fa-calendar-alt"></i> <strong> October</strong> </a>
-                  </li>
-                  <li>
-                    <a href="" class="mnth_c border-bottom"><i class="fas fa-calendar-alt"></i> <strong> November </strong> </a> 
-                  </li>
-                   <li>
-                    <a href="" class="mnth_c border-bottom"><i class="fas fa-calendar-alt"></i> <strong> December</strong> </a>
-                  </li>
-                </ul>
-              </div>
-              </div>
-              <div class="col-md-9">
-                  <div class="card">
-                      <div class="card-header row border-bottom py-1">
-                        <div class="col-md-6">
-                          <span><i class="fas fa-calendar-alt"></i> <strong> Month Name</strong></span> 
-                         </div>
-                          <!-- <div class="col-md-6 text-right"><button class="btn btn-success rounded-0"><i class="fa fa-plus" aria-hidden="true"></i> Add Time Manually</button></div> -->
-                      </div>
-                      <div class="p-2 pt-3 ">
-                       <table id="example" class="display nowrap " style="width:100%">
+    <div class="col-md-3">
+      <div class="card">
+        <ul class="mt nav-pills month_tbs nav-stacked navbar-custom-nav list-unstyled">
+            <?php
+            foreach ($all_holiday_list as $key => $v_holiday_list):
+                $year = date('Y');
+                $month_name = date('F', strtotime($year . '-' . $key)); // get full name of month by date query
+                ?>
+                <li class="month_sel <?php
+                if ($current_month == $key) {
+                    echo 'active';
+                }
+                ?>">
+                    <a aria-expanded="<?php
+                    if ($current_month == $key) {
+                        echo 'true';
+                    } else {
+                        echo 'false';
+                    }
+                    ?>" data-toggle="tab" class="mnth_c border-bottom " href="#<?php echo $month_name ?>">
+                        <i class="fa fa-fw fa-calendar"></i> <?php echo $month_name; ?> </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+      </div>
+    </div>
+<script>
+$(document).on("click",".month_sel",function(){
+  $(".month_sel").removeClass("active");
+  $(".mnth_c").removeClass("active");
+  $(this).addClass("active");
+})
+</script>
+<div class="col-md-9">
+  <div class="card">
+        <div class="tab-content pl0">
+            <?php
+            foreach ($all_holiday_list as $key => $v_holiday_list):
+                $year = date('Y');
+                $month_name = date('F', strtotime($year . '-' . $key)); // get full name of month by date query
+                ?>
+                <div id="<?php echo $month_name ?>" class="tab-pane <?php
+                if ($current_month == $key) {
+                    echo 'active';
+                }
+                ?>">
+                    <div class="card-header row border-bottom py-1">
+                        <div class="panel-heading">
+                            <div class="panel-title">
+                                <strong><i class="fa fa-calendar"></i> <?php echo $month_name; ?></strong>
+                            </div>
+
+                        </div>
+                        <!-- Table -->
+                        <table class="table table-bordered table-hover">
                             <thead>
-                                <tr>
-                                    <th>Event Name</th>
-                                    <th>Start Date</th>
-                                    <th>End Date </th>
-                                    <th>Color</th>
-                                    <th>Action</th>
-                                   
-                                    
-                                </tr>
+                            <tr>
+                                <th>Event Name</th>
+                                <th class="col-sm-2">Start Date</th>
+                                <th class="col-sm-2">End Date</th>
+                                <th class="col-sm-1">Color</th>
+                                <?php if (!empty($edited) || !empty($deleted)) { ?>
+                                    <th class="col-sm-2">Action</th>
+                                <?php } ?>
+                            </tr>
                             </thead>
                             <tbody>
-                          
-                                  <tr>
-                                    <td>Tiger Nixon</td>
-                                    <td>2011/04/25</td>
-                                    <td>Edinburgh</td>
-                                    <td>Edinburgh</td>
-                                    <td>Tiger Nixon</td>
-                                  </tr>  
-                             
-                                
+                            <?php $key = 1 ?>
+                            <?php if (!empty($v_holiday_list)): foreach ($v_holiday_list as $v_holiday) : ?>
+                                <tr>
+                                    <td><?php echo $v_holiday->event_name ?></td>
+                                    <td><?=$v_holiday->start_date?></td>
+                                    <td><?=$v_holiday->end_date?></td>
+                                    <td><span style="background-color:<?= $v_holiday->color ?>"
+                                              class="color-tag"></span></td>
+                                    <?php if (!empty($edited) || !empty($deleted)) { ?>
+                                        <td>
+                                            <?php if (!empty($edited)) { ?>
+
+                                                <?php echo btn_edit_modal('admin/holiday/add_holiday/' . $v_holiday->holiday_id); ?>
+                                            <?php }
+                                            if (!empty($deleted)) { ?>
+                                                <?php echo btn_delete('admin/holiday/delete_holiday/' . $v_holiday->holiday_id); ?>
+                                            <?php } ?>
+                                        </td>
+                                    <?php } ?>
+
+                                </tr>
+                                <?php
+                                $key++;
+                            endforeach;
+                                ?>
+                            <?php else : ?>
+                                <td colspan="3">
+                                    <strong>Nothin to Display</strong>
+                                </td>
+                            <?php endif; ?>
                             </tbody>
-                           
                         </table>
-                      </div>
-                  </div>
-              </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+  </div>
             
           </div>
         </div>    
@@ -213,7 +258,7 @@
           </div>
           <div class="line"></div>
         <div class="modal-body">
-            <form>
+            <form id="add_holiday">
             
             <div class="form-group">
               <div class="row">
@@ -221,7 +266,7 @@
                   <label for="exampleInputEmail1">Event Name <sup class="text-danger">*</sup> </label>
                 </div>
                 <div class="col-sm-6">
-                  <input type="text" class="form-control" id=""  placeholder="Enter Your Event Name" required="">
+                  <input type="text" class="form-control" id="" name="event_name"  placeholder="Enter Your Event Name" required="">
                 </div>
               </div>
             </div>
@@ -231,7 +276,7 @@
                   <label for="exampleInputEmail1">Description <sup class="text-danger">*</sup> </label>
                 </div>
                 <div class="col-sm-6">
-                    <textarea rows="3" cols="42"> Enter Your Description</textarea>
+                    <textarea rows="3" cols="42" required="" name="description"></textarea>
                 </div>
               </div>
             </div>
@@ -244,7 +289,7 @@
                 </div>
                 <div class="col-sm-6">
                     <div class='input-group date form-group datetimepicker10' id='datetimepicker10'>
-                        <input type='text' class="form-control" />
+                        <input type='text' name="start_date" required="" class="form-control" />
                           <span class="input-group-addon">
                             <span ><i class="fa fa-calendar"></i></span>
                         </span>
@@ -259,7 +304,7 @@
                 </div>
                 <div class="col-sm-6">
                     <div class='input-group date form-group datetimepicker10' id='datetimepicker10'>
-                        <input type='text' class="form-control" />
+                        <input type='text' name="end_date" required="" class="form-control" />
                           <span class="input-group-addon">
                             <span ><i class="fa fa-calendar"></i></span>
                         </span>
@@ -273,46 +318,87 @@
                   <label for="exampleInputEmail1">Location </label>
                 </div>
                 <div class="col-sm-6">
-                  <input type="text" class="form-control" id=""  placeholder="Enter Your Location" required="">
+                  <input type="text" class="form-control" id="" name="location" placeholder="Enter Your Location">
                 </div>
               </div>
             </div>
-      <div class="form-group">
-              <div class="row">
-                <div class="offset-1 col-sm-3">
-                  
+            <div class="form-group" style="margin-left: 120px">
+                <label for="field-1" class="col-sm-3 control-label"></label>
+
+                <div class="color-palet col-sm-8">
+                    <span style="background-color:#83c340" class="color-tag clickable active" data-color="#83c340"></span>
+                    <span style="background-color:#29c2c2" class="color-tag clickable" data-color="#29c2c2"></span>
+                    <span style="background-color:#2d9cdb" class="color-tag clickable" data-color="#2d9cdb"></span>
+                    <span style="background-color:#aab7b7" class="color-tag clickable" data-color="#aab7b7"></span>
+                    <span style="background-color:#f1c40f" class="color-tag clickable" data-color="#f1c40f"></span>
+                    <span style="background-color:#e18a00" class="color-tag clickable" data-color="#e18a00"></span>
+                    <span style="background-color:#e74c3c" class="color-tag clickable" data-color="#e74c3c"></span>
+                    <span style="background-color:#d43480" class="color-tag clickable" data-color="#d43480"></span>
+                    <span style="background-color:#ad159e" class="color-tag clickable" data-color="#ad159e"></span>
+                    <span style="background-color:#34495e" class="color-tag clickable" data-color="#34495e"></span>
+                    <span style="background-color:#dbadff" class="color-tag clickable" data-color="#dbadff"></span>
+                    <span style="background-color:#f05050" class="color-tag clickable" data-color="#f05050"></span>
+                    <input id="color_tag" type="hidden" name="color" value="#83c340">
                 </div>
-                <div class="col-sm-6">
-                    <ul class="d-flex " style="list-style:none">
-              <li class="p-2"><div class="p-2 bg-primary"></div></li>
-            <li class="p-2"><div class="p-2 bg-danger"></div></li>
-            <li class="p-2"><div class="p-2 bg-light"></div></li>
-            <li class="p-2"><div class="p-2 bg-secondary"></div></li>
-            <li class="p-2"><div class="p-2 bg-success"></div></li>
-            <li class="p-2"><div class="p-2 bg-warning"></div></li>
-            <li class="p-2"><div class="p-2 bg-info"></div></li>
-            <li class="p-2"><div class="p-2 bg-dark"></div></li>
-            
-          </ul>
-                </div>
-              </div>
             </div>
+            <button type="submit" class="btn btn-primary text-left">Save</button>
           </form>
         </div>
         <div class="modal-footer border-top-0 modal-butn text-left">
-          <button type="button" class="btn btn-primary text-left">Save</button>
          
           </div>
       </div>
     </div>
   </div>
 
+<script type="text/javascript">
+      $("#add_holiday").submit(function(e){
+       e.preventDefault();
+          var formData= new FormData($(this)[0]);
+          $.ajax({
+            url:"<?=base_url()?>Utilities/add_holiday",
+            type:"post",
+             data:formData,
+             contentType:false,
+             processData:false,
+             cache:false,
+            success:function(response)
+            {
+              var response=JSON.parse(response);
+              if(response==1)
+              {
+                $('#add_holiday').trigger("reset");
+                swal("Successfull", "Holiday Added Successfully", "success");
+              }
+              else
+              {
+               swal("OOPS", "Something Went Wrong", "error");
+              }
+            }
+         });
+    });
 
+
+  $(document).on('click','.clickable',function(){
+    var el=$(this);
+    $( ".clickable" ).removeClass( "active" );
+    el.addClass("active");
+    var color_code=el.attr('data-color');
+    $('#color_tag').val(color_code);
+  })
+</script>
 <script type="text/javascript">
     $(function () {
         $('.datetimepicker10').datetimepicker({
             viewMode: 'years',
-            format: 'MM/YYYY'
+            format: 'YYYY-MM-DD'
+        });
+    });
+
+      $(function () {
+        $('.datetimepicker11').datetimepicker({
+            viewMode: 'years',
+            format: 'YYYY'
         });
     });
 </script>
