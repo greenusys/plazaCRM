e18c52df35fd9651c600591f2d840c59a0202ead
Deepak <shivam0474@gmail.com>
2020-02-19 15:57:53 +0530

    backups

--- application/controllers/Backupdatabase.php
+++ application/controllers/Backupdatabase.php
@@ -6,7 +6,7 @@ class Backupdatabase extends MY_Controller {
 	public function index()
 	{
 		$this->load->view('layout/header');
-		$this->load->view("pages/backup_database");
+		$this->load->view("pages/database_backup");
 		$this->load->view("layout/footer");
 	}
 	// public function backupDatabase()
