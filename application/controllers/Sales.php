fa903b17ed515904738b0ef211da3fb0491ba167
root <root@localhost.localdomain>
2020-02-26 11:18:39 +0530

    rav

--- application/controllers/Sales.php
+++ application/controllers/Sales.php
@@ -26,7 +26,57 @@ date_default_timezone_set('Asia/Kolkata');
 		else{
 			die(json_encode(array('status'=>'0')));
 		}
+	}
 
+	public function create_invoice(){
+		$invoice_date=$_POST['invoice_date'];
+		if (isset($_POST['recur_frequency'])) {
+			$_POST['recurring']="1";
+			if ($_POST['recur_frequency']=="7D") {
+				$_POST['recuring_frequency']="7";
+			}
+			elseif ($_POST['recur_frequency']=="1M") {
+				$_POST['recuring_frequency']="31";
+			}
+			elseif ($_POST['recur_frequency']=="3M") {
+				$_POST['recuring_frequency']="93";
+			}
+			elseif ($_POST['recur_frequency']=="6M") {
+				$_POST['recuring_frequency']="182";
+			}
+			elseif ($_POST['recur_frequency']=="1Y") {
+				$_POST['recuring_frequency']="365";
+			}
+			elseif ($_POST['recur_frequency']=="2Y") {
+				$_POST['recuring_frequency']="730";
+			}
+			elseif ($_POST['recur_frequency']=="3Y") {
+				$_POST['recuring_frequency']="1095";
+			}
+			else{
+				$_POST['recuring_frequency']="31";
+			}
+			$_POST['recur_next_date']=date("Y-m-d", strtotime("$invoice_date +".$_POST['recuring_frequency']." days"));
+		}
+		else{
+			$_POST['recurring']="2";
+		}
+		if(isset($_POST['everyone'])){
+			unset($_POST['everyone']);
+		}
+		$exploder=explode("-",$invoice_date);
+		$_POST['invoice_month']=$exploder[0]."-".$exploder[1];
+		$_POST['invoice_year']=$exploder[0];
+		$_POST['currency']="INR";
+		$_POST['date_saved']=date('Y-m-d H:i:s');
+		$_POST['total_tax']='{"tax_name":null,"total_tax":null}';
+		$result=$this->Sales_Model->create_invoice($_POST);
+		if (!$result) {
+			die(json_encode(array('status'=>'0','msg'=>'Invoice ID already Exists')));
+		}
+		else{
+			die(json_encode(array('status'=>'1','invoice_id'=>$result)));
+		}
 	}
 }
 ?>	
--- application/models/Sales_Model.php
+++ application/models/Sales_Model.php
@@ -2,6 +2,20 @@
 
 class Sales_Model extends MY_Model
 {
-
-
+	public function create_invoice($data){
+		$this->db->select('*');
+        $this->db->from('tbl_invoices');
+        $this->db->where('reference_no',$data['reference_no']);
+        $query = $this->db->get();
+        if ( $query->num_rows() > 0 )
+        {
+            return false;
+        }
+        else{
+	       $this->db->insert('tbl_invoices', $data);
+	       $insert_id = $this->db->insert_id();
+	       return  $insert_id;
+        }
+    }
 }
+?>
